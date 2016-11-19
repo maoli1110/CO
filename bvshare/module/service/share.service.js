7830f46f02f1e2dbbdd3486228b4662b92a0e9da
maoli1110 <848631812@qq.com>
2016-11-19 20:23:58 +0800

    bv分享独立分出来

---
+++ bvshare/module/service/share.service.js
@@ -0,0 +1,46 @@
+'use strict';
+
+bvShare.service('Share', function ($http, $q) {
+    alert('service')
+    var url = basePath;
+    alert('basePath'+basePath);
+ 
+    //分享页面的详情
+    this.shareDetail = function(coid){
+        var delay = $q.defer();
+        $.ajax({
+            type: "POST",
+            url: basePath+'rs/co/shareDetail',
+            data:coid,
+            async:false,
+            contentType:'text/plain',
+            success: function(data){
+                delay.resolve(data);
+            },
+            error:function(error){
+                delay.reject(error);
+            }
+
+        });
+        return delay.promise;
+    }
+    //分享页面的
+    this.shareOperation = function(coid){
+        var delay = $q.defer();
+        $.ajax({
+            type: "POST",
+            url: basePath+'rs/co/shareOperation',
+            data:coid,
+            async:false,
+            contentType:'text/plain',
+            success: function(data){
+                delay.resolve(data);
+            },
+            error:function(){
+                delay.reject(error);
+            }
+
+        });
+        return delay.promise;
+    }
+});
